bced8315d3ff7c377cd8a53cb95f5aae1312e4e8
Aastha2785 <aasthagupta1047@gmail.com>
2026-09-12 18:00:06 +0530

    Add files via upload
    172. factorial trailing zeros

---
+++ fact.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class fact{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=0;
+        while(n>=5){
+            x=x+n/5;
+            n=n/5;
+        }
+        System.out.println(x);
+    }
+}
